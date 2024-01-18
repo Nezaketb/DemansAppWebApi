d918b134db1741a0cbb1a773b131aaaa856ce50c
Nezaket Bağlam <180101017@ogrenci.yalova.edu.tr>
2024-01-18 20:50:43 +0300

    .

--- DemansAppWebApi/Entities/Request/RegisterRequest.cs
+++ DemansAppWebApi/Entities/Request/RegisterRequest.cs
@@ -4,6 +4,9 @@ namespace DemansAppWebApi.Entities.Request
 	public class RegisterRequest
 	{
         public string UserName { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string EmergencyPhone { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
     }
